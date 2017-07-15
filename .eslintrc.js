98926b828d5e54209615aa3f85a00c4e18555a17
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2017-07-15 19:24:50 +0100

    Add the linter config file and the gulp file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "globals": {
+      "window": true,
+      "document": true
+  },
+    "rules": {
+        "indent": ["error", "tab"],
+        "no-tabs": [0]
+    }
+};
